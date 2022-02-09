e86019519c583dbb3aad55c55260831f7559437c
Daniel Soto <dasg.1993@gmail.com>
2022-02-08 19:27:23 -0500

    set boilerplate react navigation & redux

---
+++ android/app/src/main/java/com/cameraexample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cameraexample;
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cameraexample";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+}
